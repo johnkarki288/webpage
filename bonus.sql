fcd7dff821b61072a262d9bcfbc6456bf3b65614
johnkarki288 <108653136+johnkarki288@users.noreply.github.com>
2022-12-27 12:24:47 +0545

    Add files via upload

---
+++ bonus.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 27, 2022 at 07:37 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `office`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bonus`
+--
+
+CREATE TABLE `bonus` (
+  `WORKER_REF_ID` int(11) DEFAULT NULL,
+  `BONUS_AMOUNT` int(10) DEFAULT NULL,
+  `BONUS_DATE` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bonus`
+--
+
+INSERT INTO `bonus` (`WORKER_REF_ID`, `BONUS_AMOUNT`, `BONUS_DATE`) VALUES
+(220091, 10000, '2015-02-20 09:00:00'),
+(220092, 10000, '2015-03-20 09:00:00'),
+(220093, 10000, '2015-03-20 09:00:00'),
+(220094, 10000, '2015-03-20 09:00:00'),
+(220095, 10000, '2015-03-20 09:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bonus`
+--
+ALTER TABLE `bonus`
+  ADD KEY `WORKER_REF_ID` (`WORKER_REF_ID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bonus`
+--
+ALTER TABLE `bonus`
+  ADD CONSTRAINT `bonus_ibfk_1` FOREIGN KEY (`WORKER_REF_ID`) REFERENCES `worker` (`WORKER_ID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
